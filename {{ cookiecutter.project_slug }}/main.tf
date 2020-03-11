0d6cba8ede2d3c2a0431f09611bcb332f63ac53a
Krzysztof Grodzicki <kgrodzicki@gmail.com>
2020-03-11 10:16:17 +0100

    update: template

--- {{ cookiecutter.project_slug }}/main.tf
+++ {{ cookiecutter.project_slug }}/main.tf
@@ -1,13 +1,32 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# DEPLOY {{ cookiecutter.project_name.upper() }}
+# ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
+# This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+# ----------------------------------------------------------------------------------------------------------------------
+
 data "aws_region" "current" {}
 
-provider "aws" {
+terraform {
+  required_version = ">= 0.12"
 }
+
+provider "aws" {
+  version = "~> 2.5"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# STATE BUCKET WITH LOCKING IN DB
+# ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
   backend "s3" {
+    encrypt  = true
     bucket  = "{{ cookiecutter.terraform_state_s3 }}"
     key     = "{{ cookiecutter.project_slug }}"
     region  = "{{ cookiecutter.aws_region }}"
-    encrypt = true
+    // dynamodb_table = "terraform-lock"
   }
 }
